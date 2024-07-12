f49c658404dee5b644ec796320c739631fadf5e3
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2024-07-13 01:28:30 +1000

    add home button to lyrics page

--- public/js/lyrics.js
+++ public/js/lyrics.js
@@ -1,7 +1,7 @@
-document.getElementById("home-button").addEventListener("click", function () {
+document.getElementById("home-button")?.addEventListener("click", function () {
   window.location.href = "/";
 });
 
-document.getElementById("login-button").addEventListener("click", function () {
+document.getElementById("login-button")?.addEventListener("click", function () {
   window.location.href = "/login";
 });
