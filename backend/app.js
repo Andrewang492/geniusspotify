f6ed6b7e890e42752770137c231f7da80c4ccce1
Andre Wang <andrewang87@yahoo.com.au>
2025-12-07 19:58:41 +1100

    backend has genius authentication flow complete.

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,12 @@
  * https://developer.spotify.com/documentation/web-api/tutorials/code-flow
  */
 
-var express = require('express');
-var request = require('request');
-var crypto = require('crypto');
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+var express = require("express");
+var request = require("request");
+var crypto = require("crypto");
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const {
@@ -23,6 +23,12 @@ const {
 } = require("../scripts/geniusQuery"); //TODO change path.
 // var SpotifyWebApi = require("spotify-web-api-js"); // TODO never figured out how to use, the response from functions are not http responses.
 // const spotifyApi = new SpotifyWebApi();//
+var app = express();
+
+app
+  .use(express.static(__dirname + "/public"))
+  .use(cors())
+  .use(cookieParser());
 
 const baseurl = `http://${process.env.HOST}:${process.env.PORT}`;
 // const baseurl = process.env.BASE_URL;
@@ -30,44 +36,40 @@ const frontendurl = process.env.FRONTEND_URL;
 var client_id = process.env.CLIENT_ID; // your clientId
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = `${baseurl}/callback`;
+
 const g_client_accessToken = process.env.G_CLIENT_ACCESS_TOKEN;
-
-
+var g_client_id = process.env.G_CLIENT_ID;
+var g_client_secret = process.env.G_CLIENT_SECRET;
+var g_redirect_uri = `${baseurl}/gcallback`;
 
 const generateRandomString = (length) => {
-  return crypto
-  .randomBytes(60)
-  .toString('hex')
-  .slice(0, length);
-}
+  return crypto.randomBytes(60).toString("hex").slice(0, length);
+};
 
-var stateKey = 'spotify_auth_state';
+var stateKey = "spotify_auth_state";
 
-var app = express();
-
-app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
-
-app.get('/login', function(req, res) {
-
+//------------------------------
+// Spotify Authentication
+//------------------------------
+app.get("/login", function (req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+  var scope = "user-read-private user-read-email user-read-playback-state";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
 });
 
-app.get('/callback', function(req, res) {
-
+app.get("/callback", function (req, res) {
   // your application requests refresh and access tokens
   // after checking the state parameter
 
@@ -76,92 +78,183 @@ app.get('/callback', function(req, res) {
   var storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
   } else {
     res.clearCookie(stateKey);
     var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
+      url: "https://accounts.spotify.com/api/token",
       form: {
         code: code,
         redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
+        grant_type: "authorization_code",
       },
       headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
       },
-      json: true
+      json: true,
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+          refresh_token = body.refresh_token;
 
         var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
+          url: "https://api.spotify.com/v1/me",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
         };
 
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
+        request.get(options, function (error, response, body) {
           console.log(body);
         });
 
         // we can also pass the token to the browser to make requests from there !!!!!!!!!!!
-        res.redirect(`${frontendurl}/#` +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+        res.redirect(
+          `${frontendurl}/#` +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
       } else {
-        res.redirect(`${frontendurl}/#` +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
+        res.redirect(
+          `${frontendurl}/#` +
+            querystring.stringify({
+              error: "invalid_token",
+            })
+        );
       }
     });
   }
 });
 
-app.get('/refresh_token', function(req, res) {
-
+app.get("/refresh_token", function (req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 
-      'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) 
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
     },
     form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
     },
-    json: true
+    json: true,
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token,
-          refresh_token = body.refresh_token;
+        refresh_token = body.refresh_token;
       res.send({
-        'access_token': access_token,
-        'refresh_token': refresh_token
+        access_token: access_token,
+        refresh_token: refresh_token,
       });
     }
   });
 });
 
 //------------------------------
-// Spotify Calls 
+// Genius Authentication
+//------------------------------
+const gStateKey = "genius_auth_state";
+app.get("/glogin", function (req, res) {
+  var state = generateRandomString(16);
+  res.cookie(gStateKey, state);
+  var scope = "me";
+
+  // your application requests authorization
+  res.redirect(
+    "https://api.genius.com/oauth/authorize?" +
+      querystring.stringify({
+        client_id: g_client_id,
+        scope: scope,
+        redirect_uri: g_redirect_uri,
+        state: state,
+        response_type: "code",
+      })
+  );
+});
+
+app.get("/gcallback", function (req, res) {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[gStateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
+  } else {
+    res.clearCookie(gStateKey);
+    var authOptions = {
+      url: "https://api.genius.com/oauth/token",
+      form: {
+        code: code,
+        client_secret: g_client_secret,
+        grant_type: "authorization_code",
+        client_id: g_client_id,
+        redirect_uri: g_redirect_uri,
+        response_type: "code",
+      },
+      // headers: {
+      //   'content-type': 'application/x-www-form-urlencoded',
+      //   Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      // },
+      // json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        // we can also pass the token to the browser to make requests from there !!!!!!!!!!!
+        res.redirect(
+          `${frontendurl}/#` +
+            querystring.stringify({
+              g_access_token: access_token,
+              g_refresh_token: refresh_token,
+            })
+        );
+      } else {
+        res.redirect(
+          `${frontendurl}/#` +
+            querystring.stringify({
+              error: "invalid_token",
+            })
+        );
+      }
+    });
+  }
+});
+
+app.get("/refresh_token", function (req, res) {});
+
+//------------------------------
+// Spotify Calls
 //------------------------------
 
-app.get('/np', (req, res) => {
+app.get("/np", (req, res) => {
   const access_token = req.query.access_token;
 
   // Get currently playing
@@ -170,17 +263,17 @@ app.get('/np', (req, res) => {
       Authorization: "Bearer " + access_token,
     },
   })
-  .then((fetchRes) => fetchRes.json())
-  .then((data) =>  res.send(data))
-  .catch((e) => res.send(e.toString()))
+    .then((fetchRes) => fetchRes.json())
+    .then((data) => res.send(data))
+    .catch((e) => res.send(e.toString()));
 });
 
 //------------------------------
 // Genius Calls
 // -----------------------------
 
-// Searches, then returns song information for the search. 
-app.get('/genius/search', (req, res) => {
+// Searches, then returns song information for the search.
+app.get("/genius/search", (req, res) => {
   const queryString = req.query.queryString;
   fetch(
     `https://api.genius.com/search?` +
@@ -191,46 +284,41 @@ app.get('/genius/search', (req, res) => {
       },
     }
   )
-  .then((geniusFetchRes) => geniusFetchRes.json())
-  .then((object) => {
+    .then((geniusFetchRes) => geniusFetchRes.json())
+    .then((object) => {
+      const songId = getGeniusBestMatchSongId(object, queryString);
 
-    const songId = getGeniusBestMatchSongId(object, queryString);
-
-    // Get actual song response
-    return fetch(`https://api.genius.com/songs/${songId}?text_format=plain`, {
-      headers: {
-        Authorization: "Bearer " + g_client_accessToken,
-      },
-    });
-  })
-  .then((geniusFetchRes) => geniusFetchRes.json())
-  .then((object) => {
-    res.send(object.response)
-  })
-  .catch((e) => res.send(e.toString()))
+      // Get actual song response
+      return fetch(`https://api.genius.com/songs/${songId}?text_format=plain`, {
+        headers: {
+          Authorization: "Bearer " + g_client_accessToken,
+        },
+      });
+    })
+    .then((geniusFetchRes) => geniusFetchRes.json())
+    .then((object) => {
+      res.send(object.response);
+    })
+    .catch((e) => res.send(e.toString()));
 });
 
-
-
 // Using a song id, get referents.
-app.get('/genius/referents', (req, res) => {
+app.get("/genius/referents", (req, res) => {
   const songId = req.query.songId;
   fetch(
-    `https://api.genius.com/referents?` +
-      querystring.stringify({ q: songId }),
+    `https://api.genius.com/referents?` + querystring.stringify({ q: songId }),
     {
       headers: {
         Authorization: "Bearer " + g_client_accessToken,
       },
     }
   )
-  .then((geniusFetchRes) => geniusFetchRes.json())
-  .then((object) => {
-    res.send(object.response)
-  })
-  .catch((e) => res.send(e.toString()))
+    .then((geniusFetchRes) => geniusFetchRes.json())
+    .then((object) => {
+      res.send(object.response);
+    })
+    .catch((e) => res.send(e.toString()));
 });
-
 
 console.log(`Listening on ${baseurl}`);
 app.listen(process.env.PORT, `${process.env.HOST}`);
